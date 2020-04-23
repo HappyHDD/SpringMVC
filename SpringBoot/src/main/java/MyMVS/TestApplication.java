ee3e3107f3bf336efc27151b58db1e779a8d778b
HDD <jeant@inbox.ru>
2020-04-23 18:44:11 +0300

    Spring Boot

---
+++ SpringBoot/src/main/java/MyMVS/TestApplication.java
@@ -0,0 +1,13 @@
+package MyMVS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+
+}
